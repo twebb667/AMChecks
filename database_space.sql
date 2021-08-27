dfe9191148c8551a337c681eb9ee87233a38a6c3
twebb667 <twebb667@gmail.com>
2021-08-27 15:25:24 +0100

    Update database_space.sql

--- database_space.sql
+++ database_space.sql
@@ -5,6 +5,6 @@
     FROM   (SELECT tablespace_name, used_space, last_value(space_time) 
             OVER (PARTITION BY tablespace_name ORDER BY space_time) AS latest 
             FROM amo.am_database_space 
-              WHERE database_name = 'DOPTALL1'
+              WHERE database_name = 'ORCL_or_whatever'
             ORDER BY tablespace_name)
     WHERE space_time = latest;
